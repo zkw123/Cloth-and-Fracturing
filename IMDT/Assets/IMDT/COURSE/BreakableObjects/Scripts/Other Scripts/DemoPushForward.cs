425684fd4ace97836801cc49dc999b3648c199ea
zkw123 <906269996@qq.com>
2020-11-01 20:01:15 +0800

    test

---
+++ IMDT/Assets/IMDT/COURSE/BreakableObjects/Scripts/Other Scripts/DemoPushForward.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DemoPushForward : MonoBehaviour
+{
+    public float force = 10f;
+
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 fDir = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        fDir.Normalize();
+        GetComponent<Rigidbody>().AddForce(fDir * force);
+    }
+}
